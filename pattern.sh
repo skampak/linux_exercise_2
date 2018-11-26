507ae9d9b90e96cda24c93f87d32ae798ca4680b
skampak <csd3221@csd.uoc.gr>
2018-11-26 16:08:53 +0200

    Added a new functionality to the script

--- pattern.sh
+++ pattern.sh
@@ -2,12 +2,14 @@
 
 wget  139.91.162.50/bioinf2018/fasta.fa
 
-grep -v '^>' | grep -ve '^$' | grep -c '[-]\{1\}'
+grep -v '^>' fasta.fa | grep -ve '^$' | grep -c '[-]\{1\}'
 
-grep -v '^>' | grep -ve '^$' | grep -c '[-]\{2\}'
+grep -v '^>' fasta.fa | grep -ve '^$' | grep -c '[-]\{2\}'
 
-grep -v '^>' | grep -ve '^$' | grep -vc '[-]\{1\}'
+grep -v '^>' fasta.fa  | grep -ve '^$' | grep -vc '[-]\{1\}'
 
-grep -v '^>' | grep -ve '^$' | grep -o -i '[ATGC]_[ATGC]_[ATGC]'| wc -l
+grep -v '^>' fasta.fa  | grep -ve '^$' | grep -o -i '[ATGC]_[ATGC]_[ATGC]'| wc -l
 
-grep -v '^>' | grep -ve '^$' | grep -o '[G]\{3\}[CT][AG] | sort | uniq -ic
+grep -v '^>' fasta.fa  | grep -ve '^$' | grep -o '[G]\{3\}[CT][AG] | sort | uniq -ic
+
+grep '^>' fasta.fa  | grep -c 'hg19'
